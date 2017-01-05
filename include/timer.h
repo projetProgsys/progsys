1d0bd4ef872eee1756674e2e6af7ce0ead96b814
Chloe Pathe <cpathe@emi.u-bordeaux1.fr>
2017-01-05 11:17:02 +0100

    correction timer.h

--- include/timer.h
+++ include/timer.h
@@ -2,12 +2,10 @@
 #ifndef TIMER_IS_DEF
 #define TIMER_IS_DEF
 
-// Tyoe d'événements "utilisateur"
-enum {
-  TIMER_ONCE,
-  TIMER_REPEAT
-};
-
 SDL_TimerID timer_set (Uint32 delay, int timer_type, void *param);
+
+int timer_init(void);
+void timer_set(Uint32 delay, void *param);
+void sdl_push_event (void *param);
 
 #endif
